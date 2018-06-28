920b870e47123133a30f5b39cdc7eb74903da381
xiaomashu <1292504787@qq.com>
2018-06-28 11:09:13 +0800

    提现

--- packageTwoLeval/pages/withdraw/index.js
+++ packageTwoLeval/pages/withdraw/index.js
@@ -1,4 +1,4 @@
-// packageTwoLeval/pages/addbank/addbank.js
+// packageTwoLeval/pages/ withdraw/index.js
 Page({
 
   /**
@@ -14,7 +14,6 @@ Page({
       index: e.detail.value
     })
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
