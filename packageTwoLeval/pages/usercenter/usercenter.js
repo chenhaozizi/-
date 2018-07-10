0c16b4eacef3573e1ab09c3ad65f39d4d5d9f985
xiaomashu <1292504787@qq.com>
2018-07-10 14:27:44 +0800

    读取经销商

--- packageTwoLeval/pages/usercenter/usercenter.js
+++ packageTwoLeval/pages/usercenter/usercenter.js
@@ -1,21 +1,48 @@
-import HttpUtil from '../../../lib/trilobite/core/httputil.js'
+import HttpUtil from '../../../lib/trilobite/core/rsHttps.js'
 // import goPage from '../../../lib/page.js'
 //获取应用实例
 var app = getApp();
 let self,comp;
+class findInfoDao {
+  constructor() {
+    this.http = new HttpUtil(app);
+    this.http.addResultListener(this.result);
 
+  }
+  result = (res) => {
+    if (this.callback) {
+      this.callback(res);
+    }
+  }
+  /**
+   * 加载接口
+   */
+  load = () => {
+    this.http.post("/RsMember/FindMemberInfo", { memberId: wx.getStorageSync("memberId") })
+
+  }
+}
 /**
  * 页面控制器
  */
 class PageController {
   constructor() {
     comp = this;
-   
+    comp.findInfoDao = new findInfoDao();
+    comp.findInfoDao.callback = this.findInfoDao_callback;
+  }
+  findInfoDao_callback = (res) =>{
+    if(res.data.code == 200){
+      self.setData({
+        memberType: res.data.data.memberTypeName
+      })
+    }
   }
   data={
     headUrl:'',
     nickname:'',
     userInfo: '',
+    memberType:""
   }
   onShow=function(){
     this.getUserInfo();
@@ -30,6 +57,7 @@ class PageController {
             that.setData({
               userInfo: res.userInfo
             });
+            comp.findInfoDao.load()
           }
         })
       }
