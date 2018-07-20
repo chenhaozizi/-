3acae8cb3431227fdd79dfd89dfefadea87bce58
xiaomashu <1292504787@qq.com>
2018-07-20 17:24:48 +0800

    首页视频

--- pages/user_info/user_info.js
+++ pages/user_info/user_info.js
@@ -1,4 +1,5 @@
 // pages/getuserInfo/user_info.js
+let self;
 var app = getApp();
 Page({
 
@@ -6,7 +7,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    video_src: "http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400", //引导视频地址
+    video_src: "", //引导视频地址
     video_hidden: true, //隐藏视频引导页
 
   },
@@ -47,6 +48,24 @@ Page({
       })
     };
 
+  },
+  onLoad: function () {
+    self = this;
+    wx.request({
+      method: "POST",
+      url: 'https://mingjiu-api.conpanda.cn/front_v1/Video/FindHome',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded' // 默认值
+      },
+      success: function (res) {
+        console.log(res)
+       if(res.data.code == 200){
+         self.setData({
+           video_src: res.data.data.videoUrl
+         })
+        }
+      }
+    })
   },
   // 引导视频结束
   video_end: function() {
